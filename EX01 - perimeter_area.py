23a254414c8d1272281c16783f7f45255daccdd0
Ellen Gomes <78104233+EllenCGomes@users.noreply.github.com>
2021-11-02 15:56:37 -0300

    Add files via upload

---
+++ EX01 - perimeter_area.py
@@ -0,0 +1,10 @@
+# receive the square side value
+side = input("Enter the corresponding value for the square side:")
+
+# calculate perimeter
+perimeter = (int(side))**2
+
+# calculate area
+area = (int(side))*4
+
+print("perimeter: {} - area: {}".format(perimeter, area))
